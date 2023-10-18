2f56bbb3a86e1faac47b7986d9c416361b84847b
Priyanka-390 <psr82220@gmail.com>
2023-10-18 12:49:45 +0530

    first commit

---
+++ assets/js/doc.js
@@ -0,0 +1,21 @@
+
+// nav-bar
+function openNav() {
+    document.getElementById("navbar").classList.toggle("end-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.getElementById("menubtn-icon").classList.toggle("cross")
+}
+
+// back to top-------
+window.addEventListener('scroll', function () {
+    let backtotop = document.getElementById("backtotop")
+    if (window.scrollY > 300) { backtotop.style.display = 'block' }
+    else { backtotop.style.display = 'none' }
+})
+
+// preloader---------
+setTimeout(() => {
+    document.getElementById("preloder").classList.add("opacity-0");
+    document.getElementById("preloder").classList.add("pointer_event_none");
+    document.body.classList.remove("overflow-hidden")
+}, 3000);
